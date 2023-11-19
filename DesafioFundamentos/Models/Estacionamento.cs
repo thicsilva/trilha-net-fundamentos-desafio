c9981047804ad4d7ddb42a0da217fa7f7f3bdadf
Thiago Silva <thicsilva@yahoo.com.br>
2023-11-19 17:27:48 -0300

    tarefa #4 - Melhorias na validação do código

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,34 +16,40 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             var placa = Console.ReadLine();
-            _veiculos.Add(placa);
+            if (!string.IsNullOrEmpty(placa))
+                _veiculos.Add(placa.ToUpper());
+            else
+                Console.WriteLine("Obrigatório informar a placa!");
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
             var placa = Console.ReadLine();
-
-            // Verifica se o veículo existe
-            if (_veiculos.Any(x => string.Equals(x, placa, StringComparison.CurrentCultureIgnoreCase)))
-            {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
-                // Validando entrada do usuário
-                if (int.TryParse(Console.ReadLine(), out var horas))
-                {
-                    var valorTotal = _precoInicial + _precoPorHora * horas;
-
-                    _veiculos.Remove(placa);
-
-                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
-                } else 
-                    Console.WriteLine("O valor inserido para horas não é válido!");
-                
-            }
+            if (string.IsNullOrEmpty(placa))
+                Console.WriteLine("Obrigatório informar a placa!");
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                // Verifica se o veículo existe
+                if (_veiculos.Any(x => string.Equals(x, placa, StringComparison.CurrentCultureIgnoreCase)))
+                {
+                    Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+
+                    // Validando entrada do usuário
+                    if (int.TryParse(Console.ReadLine(), out var horas))
+                    {
+                        var valorTotal = _precoInicial + _precoPorHora * horas;
+
+                        _veiculos.Remove(placa?.ToUpper());
+
+                        Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                    } else 
+                        Console.WriteLine("O valor inserido para horas não é válido!");
+                
+                }
+                else
+                    Console.WriteLine(
+                        "Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
             }
         }
 
